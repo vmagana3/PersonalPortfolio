e418656b5f6a4e1dac577c46730763d8e5b53a0c
vmagana3 <vmagana3@ucol.mx>
2021-04-24 12:26:45 -0500

    Initial Setup

---
+++ js/colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  oxfordBlue: "#00162dff",
+  cobaltBlue: "#0147a6ff",
+  cornflowerBlue: "#7898ffff",
+  lavenderWeb: "#dfe7faff",
+  silverChalice: "#ada9a6ff",
+  timberwolf: "#ddd9d6ff",
+  platinum: "#e8e3e0ff",
+  bakerMillerPink: "#ff96b2ff",
+};
+
+export default Colors;
