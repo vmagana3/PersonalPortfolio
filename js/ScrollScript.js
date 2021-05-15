2ab01a84ff2f829f82e88a7273e644e7a386208d
vmagana3 <vmagana3@ucol.mx>
2021-05-15 17:07:50 -0500

    Scroll to section completed

---
+++ js/ScrollScript.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll("a.linkNavbar");
+
+links.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
+    let myElement = document.getElementById(item.getAttribute("data-link"));
+    const myTop = myElement.offsetTop;
+    window.scroll({
+      top: myTop - 200,
+      behavior: "smooth",
+    });
+    var clickedItem = item.parentNode;
+    clickedItem.style.backgroundColor = "red";
+  });
+});
